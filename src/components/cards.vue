8c6c68899b8a7a4a3939b6c1e3a00e0c7d75b389
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-11-11 18:17:32 +0100

    BONUS complete

---
+++ src/components/cards.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="bg_primary">
+      <div class="cards_contain contain d-flex">
+      <div class="card d-flex align-items-center">
+        <img src="../assets/img/buy-comics-digital-comics.png" alt="" />
+        <p class="card_text">DIGITAL COMICS</p>
+      </div>
+
+      <div class="card d-flex align-items-center">
+        <img src="../assets/img/buy-comics-merchandise.png" alt="" />
+        <p class="card_text">DC MERCHANDISE</p>
+      </div>
+
+      <div class="card d-flex align-items-center">
+        <img src="../assets/img/buy-comics-subscriptions.png" alt="" />
+        <p class="card_text">SUBSCRIPTION</p>
+      </div>
+
+      <div class="card d-flex align-items-center">
+        <img src="../assets/img/buy-comics-shop-locator.png" alt="" />
+        <p class="card_text">COMIC SHOP LOCATOR</p>
+      </div>
+
+      <div class="card d-flex align-items-center">
+        <img src="../assets/img/buy-dc-power-visa.svg" alt="" />
+        <p class="card_text">DC POWER VISA</p>
+      </div>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/main.vue
+++ src/components/main.vue
@@ -8,38 +8,18 @@
       </div>
     </div>
 
-    <div class="bg_primary">
-      <div class="cards_contain contain d-flex">
-      <div class="card d-flex align-items-center">
-        <img src="../assets/img/buy-comics-digital-comics.png" alt="" />
-        <p class="card_text">DIGITAL COMICS</p>
-      </div>
-
-      <div class="card d-flex align-items-center">
-        <img src="../assets/img/buy-comics-merchandise.png" alt="" />
-        <p class="card_text">DC MERCHANDISE</p>
-      </div>
-
-      <div class="card d-flex align-items-center">
-        <img src="../assets/img/buy-comics-subscriptions.png" alt="" />
-        <p class="card_text">SUBSCRIPTION</p>
-      </div>
-
-      <div class="card d-flex align-items-center">
-        <img src="../assets/img/buy-comics-shop-locator.png" alt="" />
-        <p class="card_text">COMIC SHOP LOCATOR</p>
-      </div>
-
-      <div class="card d-flex align-items-center">
-        <img src="../assets/img/buy-dc-power-visa.svg" alt="" />
-        <p class="card_text">DC POWER VISA</p>
-      </div>
-    </div>
-    </div>
+    <cards/>
   </main>
 </template>
 
 <script>
+import cards from './cards.vue'
+
+export default {
+  components: {
+    cards
+  }
+}
 </script>
 
 <style lang="scss">
