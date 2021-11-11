347a95db0acc32f04002fb5daf6ed7ceee747e41
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-11-11 15:02:10 +0100

    nav with logo bug

--- dc/src/App.vue
+++ dc/src/App.vue
@@ -1,12 +1,29 @@
 <template>
   <div id="app">
+    <SiteHeader />
   </div>
 </template>
 
 <script>
 
+import SiteHeader from './components/header.vue'
+
+export default {
+  name: 'app',
+  components: {
+    SiteHeader
+  }
+}
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Kalam", cursive;
+  font-family: "Montserrat", sans-serif;
+}
+
 
 </style>
---
+++ dc/src/components/header.vue
@@ -0,0 +1,63 @@
+<template>
+  <header class="d-flex jc-between align-items-center">
+    <div class="logo_contain">
+      <img src="../assets/img/dc-logo.png" alt="" />
+    </div>
+
+    <nav>
+      <ul class="d-flex">
+        <li><a href="">CHARACTER</a></li>
+        <li><a href="">COMICS</a></li>
+        <li><a href="">MOVIES</a></li>
+        <li><a href="">TV</a></li>
+        <li><a href="">GAMES</a></li>
+        <li><a href="">COLLECTIBLES</a></li>
+        <li><a href="">VIDEOS</a></li>
+        <li><a href="">FANS</a></li>
+        <li><a href="">NEWS</a></li>
+        <li><a href="">SHOP</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+.d-flex {
+  display: flex;
+}
+
+.jc-between {
+  justify-content: space-between;
+}
+
+.align-items-center {
+  align-items: center;
+}
+
+header {
+  ul {
+    li {
+      list-style: none;
+      margin-left: 1rem;
+      font-weight: 600;
+
+      &:hover {
+        border-bottom: 10px solid blue;
+      }
+
+      a { 
+        line-height: 107px;
+        text-decoration: none;
+        color: black;
+
+        &:hover {
+          color: blue;
+        }
+      }
+    }
+  }
+}
+</style>
