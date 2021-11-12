a8e009afb715adc201a3dd3250f289f5959a8791
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-11-12 12:07:14 +0100

    add btn for current series

--- src/components/jumbo.vue
+++ src/components/jumbo.vue
@@ -1,16 +1,31 @@
 <template>
-  <div class="jumbo"></div>
+  <div class="jumbo">
+    <div class="contain">
+      <div class="currentSeries">CURRECT SERIES</div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style lang="scss">
+@import '../assets/scss/variables.scss';
+
 .jumbo {
   height: 500px;
   background-image: url('../assets/img/jumbotron.jpg');
+  position: relative;
+}
+
+.currentSeries {
+  width: fit-content;
+  padding: 1.1rem 1.5rem;
+  background-color: $principal_color;
+  font-size: 1.5rem;
+  font-weight: 500;
+  position: absolute;
+  bottom: -30px;
 }
 </style>
